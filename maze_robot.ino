340b01cf90207e19add928a644245e04d7a92d71
19G12 <139550988+19G12@users.noreply.github.com>
2024-12-10 20:49:28 +0530

    KTYN edit 10-12

--- maze_robot.ino
+++ maze_robot.ino
@@ -1,9 +1,10 @@
-const int RMN = 10; // Left motor backward
-const int RMP = 11; // Left motor forward
-const int LMN = 12; // Right motor forward
-const int LMP = 13; // Right motor backward 
+// Motor Pins
+const int RMN = 13; // Left motor backward
+const int RMP = 12; // Left motor forward
+const int LMN = 11; // Right motor forward
+const int LMP = 10; // Right motor backward
 
-// Define sensor pins
+// Sensor Pins
 const int rightmost = 0;
 const int right = 1;
 const int middle = 7;
@@ -11,20 +12,34 @@ const int left = 3;
 const int leftmost = 4;
 const int back = 2;
 
-// Define global variables
-int const TURN_DELAY = 2000;
+// Global Variables
+const int motorSpeed = 150; // PWM speed (0-255)
 int sensorValues[6];
 
-// Setup method
+// Constants
+const int TURN_DELAY = 200; // Delay for turning
+
+// Function Prototypes
+void forward();
+void stopMotors();
+void turnRight();
+void turnLeft();
+void turnBack();
+void readSensorValues();
+bool isStraightLineDetected();
+bool isLeftTurnRequired();
+void printSensorStates();
+
 void setup() {
   Serial.begin(9600);
-  // Initialize motor pins as outputs
+
+  // Motor pins as outputs
   pinMode(LMN, OUTPUT);
   pinMode(LMP, OUTPUT);
   pinMode(RMP, OUTPUT);
   pinMode(RMN, OUTPUT);
 
-  // Initialize sensor pins as inputs
+  // Sensor pins as inputs
   pinMode(rightmost, INPUT);
   pinMode(right, INPUT);
   pinMode(middle, INPUT);
@@ -32,114 +47,113 @@ void setup() {
   pinMode(leftmost, INPUT);
   pinMode(back, INPUT);
 
-  // Ensure motors are stopped initially
-  stop();
+  stopMotors(); // Ensure motors are stopped initially
 }
 
-// Loop method
 void loop() {
-  lineFollowing(sensorValues); 
+  readSensorValues();
+  printSensorStates();
 
-  
-  Serial.print(sensorValues[0]);
-  Serial.print(" ");
-  Serial.print(sensorValues[1]);
-  Serial.print(" ");
-  Serial.print(sensorValues[2]);
-  Serial.print(" ");
-  Serial.print(sensorValues[3]);
-  Serial.print(" ");
-  Serial.print(sensorValues[4]);
-  Serial.print(" ");
-  Serial.println(sensorValues[5]);
-
-    // Handle: Left + Left-T + T
-    if ((sensorValues[0] && sensorValues[1]  && !sensorValues[3] && !sensorValues[4] && sensorValues[5]) || (sensorValues[0] && sensorValues[1] && !sensorValues[2]  && sensorValues[3] && sensorValues[4] && sensorValues[5])){
-        stop();
-          delay(50);
-          turnLeft();
-          while (!(sensorValues[0] && sensorValues[1] && sensorValues[2])){
-            Serial.println("Turning left");
-            lineFollowing(sensorValues);
-            delay(10);
-           }
-          forward();
-          delay(100);
-          while (sensorValues[0] && sensorValues[1] && sensorValues[2]){
-            Serial.println("Forward");
-            lineFollowing(sensorValues);
-           }
-    }
-    // Handle: Straight + T-right
-    else if(!sensorValues[0] && !sensorValues[1]  && sensorValues[2]  && sensorValues[5]  &&(( !sensorValues[3] && !sensorValues[4]) || ( sensorValues[3] && sensorValues[4]))){
-        forward();
-    }
-    else if (!sensorValues[0] && !sensorValues[1] && !sensorValues[2] && sensorValues[5] && !sensorValues[3] && !sensorValues[4]) {
-      // Change here
-    }
-    else if(!sensorValues[0] && !sensorValues[1] && !sensorValues[2] && sensorValues[5] && sensorValues[3] && sensorValues[4]){
-        stop();
-        delay(50);
-        
-        turnRight();
-        while (!(!sensorValues[0] && !sensorValues[1] && sensorValues[2] && sensorValues[5] && sensorValues[3] && sensorValues[4]) ){
-          Serial.println("Turning right");
-          lineFollowing(sensorValues);
-          delay(10);
-         }
-        forward();
-        while ((!sensorValues[0] && !sensorValues[1] && sensorValues[2] && sensorValues[5] && sensorValues[3] && sensorValues[4])){
-          Serial.println("Forward");
-          lineFollowing(sensorValues);
-         }
-    }
-    else {
-        stop();
-        delay(500);
-      }
+  // Line following logic
+  if (isStraightLineDetected()) {
+    forward();
+  } else if (isLeftTurnRequired()) {
+    stopMotors();
+    delay(TURN_DELAY);
+    turnLeft();
+    waitForCondition(isStraightLineDetected);
+  } else if (sensorValues[5]) { // Back sensor detects the line
+    stopMotors();
+    delay(250);
+    turnBack();
+    delay(2000);
+    stopMotors();
+  } else {
+    stopMotors(); // Stop if no valid condition is met
+    delay(500);
+  }
 }
 
-// Function to read sensor values
-void lineFollowing(int sensorValues[]) {
-  
-  sensorValues[0] = !digitalRead(leftmost);
-  sensorValues[1] = !digitalRead(left);
-  sensorValues[2] = !digitalRead(middle);
-  sensorValues[3] = !digitalRead(right);
-  sensorValues[4] = !digitalRead(rightmost);
-  sensorValues[5] = !digitalRead(back);
-
+// Function to read sensor values (Digital)
+void readSensorValues() {
+  sensorValues[0] = digitalRead(leftmost);
+  sensorValues[1] = digitalRead(left);
+  sensorValues[2] = digitalRead(middle);
+  sensorValues[3] = digitalRead(right);
+  sensorValues[4] = digitalRead(rightmost);
+  sensorValues[5] = digitalRead(back);
 }
 
-// Motor control functions
+// Utility Functions for Motor Control
 void forward() {
-  Serial.println("Forward");
+  Serial.println("Moving Forward");
+  analogWrite(LMP, motorSpeed);
+  analogWrite(RMP, motorSpeed);
   digitalWrite(LMN, LOW);
-  digitalWrite(LMP, HIGH);
-  digitalWrite(RMP, HIGH);
+  digitalWrite(RMN, LOW);
+}
+
+void stopMotors() {
+  Serial.println("Stopping");
+  digitalWrite(LMN, LOW);
+  digitalWrite(LMP, LOW);
+  digitalWrite(RMP, LOW);
   digitalWrite(RMN, LOW);
 }
 
 void turnRight() {
-  Serial.println("Turn Right");
-  digitalWrite(LMP, HIGH);
+  Serial.println("Turning Right");
+  analogWrite(LMP, motorSpeed);
   digitalWrite(LMN, LOW);
-  digitalWrite(RMP, LOW);
-  digitalWrite(RMN, HIGH);
+  analogWrite(RMP, 0);
+  analogWrite(RMN, motorSpeed);
 }
 
 void turnLeft() {
-  Serial.println("Turn Left");
-  digitalWrite(LMN, HIGH);
-  digitalWrite(LMP, LOW);
-  digitalWrite(RMP, HIGH);
+  Serial.println("Turning Left");
+  analogWrite(LMP, 0);
+  analogWrite(LMN, motorSpeed);
+  analogWrite(RMP, motorSpeed);
   digitalWrite(RMN, LOW);
 }
 
-void stop() {
-  Serial.println("Stop");
+void turnBack() {
+  Serial.println("Turning Back");
+  analogWrite(LMP, motorSpeed);
   digitalWrite(LMN, LOW);
-  digitalWrite(LMP, LOW);
-  digitalWrite(RMP, LOW);
+  analogWrite(RMP, motorSpeed);
   digitalWrite(RMN, LOW);
+}
+
+// Condition Check Helper
+bool isStraightLineDetected() {
+  return sensorValues[2] && (sensorValues[1] || sensorValues[3]);
+}
+
+bool isLeftTurnRequired() {
+  return sensorValues[0] && sensorValues[1] && !sensorValues[2];
+}
+
+// Debugging Helper
+void printSensorStates() {
+  Serial.print("Sensors: ");
+  for (int i = 0; i < 6; i++) {
+    Serial.print(sensorValues[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+// Wait for a specific condition with timeout
+void waitForCondition(bool (*condition)()) {
+  unsigned long startTime = millis();
+  while (!condition()) {
+    if (millis() - startTime > 1000) { // Timeout after 1 second
+      Serial.println("Timeout waiting for condition");
+      stopMotors();
+      break;
+    }
+    readSensorValues();
+    delay(10);
+  }
 }
